56a41d1253d93950361de52887ea11e376d805f4
Trainer <admin@example.com>
2024-09-01 16:35:55 +0100

    Routes and Bootstrap support.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListingPageComponent } from './pages/listing-page/listing-page.component';
+import { PassengerInfoPageComponent } from './pages/passenger-info-page/passenger-info-page.component';
+import { ReviewBookingPageComponent } from './pages/review-booking-page/review-booking-page.component';
+import { BookingPageComponent } from './pages/booking-page/booking-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SearchPageComponent,
+  },
+  {
+    path: 'listing',
+    component: ListingPageComponent,
+  },
+  {
+    path: 'passenger',
+    component: PassengerInfoPageComponent,
+  },
+  {
+    path: 'review-booking',
+    component: ReviewBookingPageComponent,
+  },
+  {
+    path: 'booking',
+    component: BookingPageComponent,
+  },
+];
---
+++ src/app/pages/listing-page/listing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-listing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './listing-page.component.html',
+  styleUrl: './listing-page.component.css'
+})
+export class ListingPageComponent {
+
+}
