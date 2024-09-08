ff7995a925af595755906f21fcc1824cd41a7aaa
Trainer <admin@example.com>
2024-09-08 16:40:04 +0100

    updated testcase

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(CalculatorService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -19,7 +28,11 @@ describe('CalculatorService', () => {
   });
 
   it('should return area of circle', () => {
-    service.pi = 3.14
+    service.pi = 3.14;
     expect(service.areaOfCirle(5)).toEqual(78.5);
   });
+
+  it("should get people data", ()=>{
+    
+  })
 });
