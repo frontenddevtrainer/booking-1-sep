12dd7543de4ec643f970b48b0060f6e561db7f65
Trainer <admin@example.com>
2024-09-08 18:14:00 +0100

    Added test.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,5 +2,17 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('Search Tickets:')
+
+    cy.get('[data-testid="search-control"]').type("Hello World");
+    cy.get('[data-testid="search-button"]').click();
+    cy.url().should("have.text", "/listing")
   })
+})
+
+
+describe("Test for Listing page", ()=>{
+    it("visit page", ()=>{
+      cy.viewport("iphone-3")
+      cy.visit("/listing/helloworld")
+    })
 })
